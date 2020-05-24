ced887617f613b73437d7577ab7b1500d4003daf
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-24 18:57:28 +0300

    Created user Public Profile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const router = express.Router();
-
 const {
   requireSignin,
-  authMiddleware,
+  authMiddleware
 } = require("../controllers/auth");
 
-const { read } = require("../controllers/user");
+const router = express.Router();
+
+const { read, publicProfile, update, photo } = require("../controllers/user");
 
 router.get("/user/profile", requireSignin, authMiddleware, read);
+router.get("/user/:username", publicProfile);
+router.put("/user/update", requireSignin, authMiddleware, update);
+router.get("/user/photo/:username", photo);
 
-module.exports = router
+module.exports = router;
 
---
+++ frontend/components/auth/ProfileUpdate.js
@@ -0,0 +1,212 @@
+import { useState, useEffect } from "react";
+
+import { getCookie, updateUser } from "../../api/auth";
+import { getProfile, update } from "../../api/user";
+import { API } from "../../config";
+
+const ProfileUpdate = () => {
+  const [values, setValues] = useState({
+    username: "",
+    name: "",
+    email: "",
+    about: "",
+    password: "",
+    error: false,
+    success: false,
+    loading: false,
+    photo: "",
+    userData: "",
+  });
+
+  const token = getCookie("token");
+  const {
+    username,
+    name,
+    email,
+    about,
+    password,
+    error,
+    success,
+    loading,
+    userData,
+  } = values;
+
+  const init = () => {
+    getProfile(token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({
+          ...values,
+          username: data.username,
+          name: data.name,
+          email: data.email,
+          about: data.about,
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleChange = (name) => (e) => {
+    const value = name === "photo" ? e.target.files[0] : e.target.value;
+    let userFormData = new FormData();
+    userFormData.set(name, value);
+
+    setValues({
+      ...values,
+      [name]: value,
+      userData: userFormData,
+      error: false,
+      success: false,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValues({ ...values, loading: true });
+
+    update(token, userData).then((data) => {
+      if (data.error) {
+        console.log("data.error", data.error);
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        updateUser(data, () => {
+          setValues({
+            ...values,
+            username: data.username,
+            name: data.name,
+            email: data.email,
+            about: data.about,
+            password: "",
+            success: true,
+            loading: false,
+          });
+        });
+      }
+    });
+  };
+
+  const profileUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="btn btn-outline-info">
+          Profile photo
+          <input
+            onChange={handleChange("photo")}
+            type="file"
+            accept="image/*"
+            hidden
+          />
+        </label>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Username</label>
+        <input
+          onChange={handleChange("username")}
+          type="text"
+          value={username}
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          value={name}
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">About</label>
+        <textarea
+          onChange={handleChange("about")}
+          type="text"
+          value={about}
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          value={password}
+          className="form-control"
+        />
+      </div>
+
+      <div>
+        {showSuccess()}
+        {showError()}
+        {showLoading()}
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          disabled={!username || !name || !email}
+        >
+          Update
+        </button>
+      </div>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-success"
+      style={{ display: success ? "" : "none" }}
+    >
+      Profile updated
+    </div>
+  );
+
+  const showLoading = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: loading ? "" : "none" }}
+    >
+      Loading...
+    </div>
+  );
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src={`${API}/user/photo/${username}`}
+              className="img img-fluid img-thumbnail mb-3"
+              style={{ maxHeight: "auto", maxWidth: "100%" }}
+              alt="user profile"
+            />
+          </div>
+
+          <div className="col-md-8 mb-5">{profileUpdateForm()}</div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ProfileUpdate;
